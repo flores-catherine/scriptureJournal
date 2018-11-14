938e4ce9715c30dc9200fb9d7f32b46875166af9
Catherine <catherine.g.flores@gmail.com>
2018-11-13 19:50:27 -0700

    Add project files.

---
+++ MyScriptureJournal/Models/dbo.Scripture.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Scripture] (
+    [ID]        INT            IDENTITY (1, 1) NOT NULL,
+    [Book]      NVARCHAR (MAX) NULL,
+    [Chapter]   INT            NOT NULL,
+    [Verse]     INT            NOT NULL,
+    [Note]      NVARCHAR (MAX) NULL,
+    [DateAdded] DATETIME2 (7)  NOT NULL,
+    CONSTRAINT [PK_Scripture] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
---
+++ MyScriptureJournal/Pages/Scriptures/Index.cshtml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyScriptureJournal.Models;
+
+namespace MyScriptureJournal.Pages.Scriptures
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MyScriptureJournal.Models.MyScriptureJournalContext _context;
+
+        public IndexModel(MyScriptureJournal.Models.MyScriptureJournalContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Scripture> Scripture { get;set; }
+        public string SearchBook { get; set; }
+        public SelectList Books { get; set; }
+        public string Book { get; set; }
+
+
+        public string BookSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+
+        public string SearchString { get; set; }
+
+
+        public async Task OnGetAsync(string searchBook, string searchString, string sortOrder)
+        {
+            BookSort = String.IsNullOrEmpty(sortOrder) ? "book" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "dateAdded";
+
+            var scriptures = from m in _context.Scripture
+                             select m;
+
+            if (!String.IsNullOrEmpty(searchBook))
+            {
+                scriptures = scriptures.Where(s => s.Book.Contains(searchBook));
+                SearchBook = searchBook;
+            }
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                scriptures = scriptures.Where(s => s.Note.Contains(searchString));
+                SearchString = searchString;
+            }
+
+            switch (sortOrder)
+            {
+                case "book":
+                    scriptures = scriptures.OrderBy(s => s.Book);
+                    break;
+                case "dateAdded":
+                    scriptures = scriptures.OrderBy(s => s.DateAdded);
+                    break;
+            }
+
+            Scripture = await scriptures.ToListAsync();
+        }
+    }
+}
